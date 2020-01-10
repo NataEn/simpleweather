ab440b426d844609855e54e84845f5509c4858df
natalie <natalie@enkin.co.il>
2020-01-10 16:02:32 +0200

    cloudes function added

--- rain.js
+++ rain.js
@@ -1,19 +1,44 @@
-let makeItRain = function() {
+const addCloudes = function(num) {
+  const cloudes = document.querySelector("div.cloudes");
+  for (let i = 1; i <= num; i++) {
+    let cloude = document.createElement("div");
+    cloude.setAttribute("class", "cloude");
+    cloude.setAttribute("style", `left:${Math.random() * 500}px`);
+    let cloudeTop1 = document.createElement("div");
+    cloudeTop1.setAttribute("class", "cloudePart");
+    cloudeTop1.setAttribute("style", "top:5px; left:-87px");
+    let cloudeTop2 = document.createElement("div");
+    cloudeTop2.setAttribute("class", "cloudePart");
+    cloudeTop2.setAttribute("style", "top:-5px; left:-95px");
+    let cloudeTop3 = document.createElement("div");
+    cloudeTop3.setAttribute("class", "cloudePart");
+    cloudeTop3.setAttribute("style", "top:-2px; left:-101px;");
+    let cloudeBottom1 = document.createElement("div");
+    cloudeBottom1.setAttribute("class", "cloudePart");
+    cloudeBottom1.setAttribute(
+      "style",
+      "width:100px;top:20px;border-radius: 50px;"
+    );
+    cloude.appendChild(cloudeBottom1);
+    cloude.appendChild(cloudeTop1);
+    cloude.appendChild(cloudeTop2);
+    cloude.appendChild(cloudeTop3);
+    cloudes.appendChild(cloude);
+  }
+};
+
+const makeItRain = function() {
   //clear out everything
   document.querySelector(".rain").innerHTMl = "";
-  // $('.rain').empty();
 
   let increment = 0;
   let drops = "";
   let backDrops = "";
 
   while (increment < 100) {
-    //couple random numbers to use for various randomizations
-    //random number between 98 and 1
     let randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
-    //random number between 5 and 2
+
     let randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
-    //increment
     increment += randoFiver;
     //add in a new raindrop with various randomizations to certain CSS properties
     drops +=
@@ -23,17 +48,9 @@ let makeItRain = function() {
       (randoFiver + randoFiver - 1 + 100) +
       "%; animation-delay: 0." +
       randoHundo +
-      "s; animation-duration: 0.5" +
+      "s; animation-duration: 0.6" +
       randoHundo +
-      's;"><div class="stem" style="animation-delay: 0.' +
-      randoHundo +
-      "s; animation-duration: 0.5" +
-      randoHundo +
-      's;"></div><div class="splat" style="animation-delay: 0.' +
-      randoHundo +
-      "s; animation-duration: 0.5" +
-      randoHundo +
-      's;"></div></div>';
+      's;"></div>';
     backDrops +=
       '<div class="drop" style="right: ' +
       increment +
@@ -41,22 +58,13 @@ let makeItRain = function() {
       (randoFiver + randoFiver - 1 + 100) +
       "%; animation-delay: 0." +
       randoHundo +
-      "s; animation-duration: 0.5" +
+      "s; animation-duration: 0.7" +
       randoHundo +
-      's;"><div class="stem" style="animation-delay: 0.' +
-      randoHundo +
-      "s; animation-duration: 0.5" +
-      randoHundo +
-      's;"></div><div class="splat" style="animation-delay: 0.' +
-      randoHundo +
-      "s; animation-duration: 0.5" +
-      randoHundo +
-      's;"></div></div>';
+      's;"></div>';
   }
   document.querySelector(".rain.front-row").innerHTML = drops;
-  // $('.rain.front-row').append(drops);
   document.querySelector(".rain.back-row").innerHTML = backDrops;
-  // $('.rain.back-row').append(backDrops);
 };
 
 makeItRain();
+addCloudes(3);
